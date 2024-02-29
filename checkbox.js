eaf9bdbf7db87ce6ffbb2cdbaaafc64ef5a4e09c
HoangLinhDo <linh.dohoang@hcmut.edu.vn>
2024-02-29 23:02:54 +0700

    Bai 2

--- checkbox.js
+++ checkbox.js
@@ -10,3 +10,16 @@ check.addEventListener('change',() => {
     }
     notification.classList.toggle('blue');
 });
+
+function submitClick(){
+    var checkbox=document.getElementById('checkbox-id');
+    var message=document.getElementById('message');
+    if(checkbox.checked){
+      message.style='color: blue'
+      message.innerHTML='Đã chọn✅';
+    }
+    else{
+      message.style='color: red'
+      message.innerHTML='Bạn cần chọn vào đây👍';
+    }
+  }
