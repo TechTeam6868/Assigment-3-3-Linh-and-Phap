1c81f3902c0a2b0fcf40dd7cb8ebb1e82449a603
Nguyen Nhat Phap <nguyennhatphapbp@gmail.com>
2024-02-29 23:23:11 +0700

    bai1 done

---
+++ bai1.js
@@ -0,0 +1,4 @@
+function buttonClick(){
+  window.alert
+        ('https://vnexpress.net/dai-hoc-cong-nghe-thong-tin-chi-10-ty-mua-sieu-may-chu-nvidia-4716666.html?fbclid=IwAR0EwZgyfv_ldA7ysSjrN9aqGs3rEvjKASQLlS8931N4d_0QylSlPMiMSxw')
+}
