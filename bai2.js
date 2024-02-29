7adc9d015c5898737d6594c3953b7b6314802eea
Nguyen Nhat Phap <nguyennhatphapbp@gmail.com>
2024-02-29 16:40:30 +0700

    Bai 2 done

---
+++ bai2.js
@@ -0,0 +1,12 @@
+function submitClick(){
+  var checkbox=document.getElementById('checkbox-id');
+  var message=document.getElementById('message');
+  if(checkbox.checked){
+    message.style='color: blue'
+    message.innerHTML='Đã chọn✅';
+  }
+  else{
+    message.style='color: red'
+    message.innerHTML='Bạn cần chọn vào đây👍';
+  }
+}
